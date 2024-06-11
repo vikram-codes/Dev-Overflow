994d48f77dd6a3fd8dffb862babc78eeab84b672
Vikram Saini <vikramjit.personal@gmail.com>
2024-06-11 17:52:08 +0530

    Added assets and layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,3 +1,4 @@
+import React from "react";
 import {
   ClerkProvider,
   SignInButton,
@@ -6,21 +7,51 @@ import {
   UserButton,
 } from "@clerk/nextjs";
 import "./globals.css";
+import { Inter, Space_Grotesk } from "next/font/google";
+import { Metadata } from "next";
+
+const inter = Inter({
+  subsets: ["latin"],
+  weight: ["100", "200", "300", "400", "500", "600", "700", "800", "900"],
+  variable: "--font-inter",
+});
+
+const spaceGrotesk = Space_Grotesk({
+  subsets: ["latin"],
+  weight: ["300", "400", "500", "600", "700"],
+  variable: "--font-spaceGrotesk",
+});
+
+export const metadata: Metadata = {
+  title: "Dev-Overflow",
+  description: "A place for developers to ask questions and share knowledge",
+  icons: {
+    icon: "/favicon.ico",
+  },
+};
+
 export default function RootLayout({
   children,
 }: {
   children: React.ReactNode;
 }) {
   return (
-    <ClerkProvider>
+    <ClerkProvider
+      appearance={{
+        elements: {
+          formButtonPrimary: "primary-gradient",
+          footerActionLink: "primary-text-gradient hover:text-primary-500",
+        },
+      }}
+    >
       <html lang="en">
-        <body>
-          <SignedOut>
+        <body className={`${inter.variable} ${spaceGrotesk.variable}`}>
+          {/* <SignedOut>
             <SignInButton />
           </SignedOut>
           <SignedIn>
             <UserButton />
-          </SignedIn>
+          </SignedIn> */}
           {children}
         </body>
       </html>
