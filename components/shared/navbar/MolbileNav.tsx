6d572f9086fd9a1f6bf82d37b274120acb1012ba
Vikram Saini <vikramjit.personal@gmail.com>
2024-06-19 15:11:41 +0530

    Installing sheet from shadcn

--- components/shared/navbar/MolbileNav.tsx
+++ components/shared/navbar/MolbileNav.tsx
@@ -1,5 +1,28 @@
 import React from "react";
+import {
+  Sheet,
+  SheetContent,
+  SheetDescription,
+  SheetHeader,
+  SheetTitle,
+  SheetTrigger,
+} from "@/components/ui/sheet";
 
 export default function MolbileNav() {
-  return <div>MolbileNav</div>;
+  return (
+    <div>
+      <Sheet>
+        <SheetTrigger>Open</SheetTrigger>
+        <SheetContent>
+          <SheetHeader>
+            <SheetTitle>Are you absolutely sure?</SheetTitle>
+            <SheetDescription>
+              This action cannot be undone. This will permanently delete your
+              account and remove your data from our servers.
+            </SheetDescription>
+          </SheetHeader>
+        </SheetContent>
+      </Sheet>
+    </div>
+  );
 }
