e5f0582162148d64595221b40c872a177f9c3c64
Vikram Saini <vikramjit.personal@gmail.com>
2024-06-19 14:33:00 +0530

    Implementing Theme Switcher

--- components/shared/navbar/Theme.tsx
+++ components/shared/navbar/Theme.tsx
@@ -13,15 +13,34 @@ import {
   MenubarShortcut,
   MenubarTrigger,
 } from "@/components/ui/menubar";
+import Image from "next/image";
 
 export default function Theme() {
   const { mode, setMode } = useTheme();
 
   return (
     <div>
-      <Menubar>
+      <Menubar className="relative border-none bg-transparent shadow-none">
         <MenubarMenu>
-          <MenubarTrigger>File</MenubarTrigger>
+          <MenubarTrigger>
+            {mode === "light" ? (
+              <Image
+                src="/assets/icons/sun.svg"
+                width={20}
+                height={20}
+                alt="Sun"
+                className="active-theme"
+              />
+            ) : (
+              <Image
+                src="/assets/icons/moon.svg"
+                width={20}
+                height={20}
+                alt="Moon"
+                className="active-theme"
+              />
+            )}
+          </MenubarTrigger>
           <MenubarContent>
             <MenubarItem>
               New Tab <MenubarShortcut>⌘T</MenubarShortcut>
