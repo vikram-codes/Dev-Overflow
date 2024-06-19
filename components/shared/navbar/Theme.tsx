fe42b337dbf5392c3291a16906cbef2b3aece995
Vikram Saini <vikramjit.personal@gmail.com>
2024-06-19 13:52:03 +0530

    Fixing import alias

--- components/shared/navbar/Theme.tsx
+++ components/shared/navbar/Theme.tsx
@@ -1,6 +1,16 @@
 import React from "react";
 
-import { useTheme } from "../../../context/ThemeProvider";
+import { useTheme } from "@/context/ThemeProvider";
+
+import {
+  Menubar,
+  MenubarContent,
+  MenubarItem,
+  MenubarMenu,
+  MenubarSeparator,
+  MenubarShortcut,
+  MenubarTrigger,
+} from "@/components/ui/menubar";
 
 export default function Theme() {
   const { mode, setMode } = useTheme();
