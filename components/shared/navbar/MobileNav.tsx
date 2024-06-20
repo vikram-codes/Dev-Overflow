de0c998634088ae4f95a241faf072e8cdc41e2ae
Vikram Saini <vikramjit.personal@gmail.com>
2024-06-20 14:13:51 +0530

    Fully Implemented MobileNav

--- components/shared/navbar/MobileNav.tsx
+++ components/shared/navbar/MobileNav.tsx
@@ -19,16 +19,29 @@ const NavContent = () => {
     <h1>
       <section className="flex h-full flex-col gap-6 pt-16">
         {sidebarLinks.map((item) => {
-          const isActive = pathname;
+          const isActive =
+            (pathname.includes(item.route) && item.route.length > 1) ||
+            pathname === item.route;
           return (
             <SheetClose asChild key={item.route}>
-              <Link href={item.route} className="">
+              <Link
+                href={item.route}
+                className={`${
+                  isActive
+                    ? "primary-gradient rounded-lg text-light-900"
+                    : "text-dark300_light900"
+                } flex items-center justify-start gap-4 bg-transparent p-4`}
+              >
                 <Image
                   src={item.imgURL}
                   alt={item.label}
                   width={20}
                   height={20}
+                  className={`${isActive ? "" : "invert-colors"}`}
                 />
+                <p className={`${isActive ? "base-bold" : "base-medium"}`}>
+                  {item.label}
+                </p>
               </Link>
             </SheetClose>
           );
