e46aa7fb1948a4c2e23802ed834478f1967a1a30
Vikram Saini <vikramjit.personal@gmail.com>
2024-06-24 13:43:24 +0530

    Working on Home

--- app/(root)/(home)/page.tsx
+++ app/(root)/(home)/page.tsx
@@ -1,5 +1,14 @@
-import { UserButton } from "@clerk/nextjs";
+import { Button } from "@/components/ui/button";
 
 export default function Home() {
-  return <div>Home</div>;
+  return (
+    <>
+      <div className="flex justify-between">
+        <h1 className="h1-bold text-dark300_light900">All Questions</h1>
+        <Button className="primary-gradient text-light-900 w-36 p-[-2px] pt-6 pb-6 ml-48">
+          Ask a Question
+        </Button>
+      </div>
+    </>
+  );
 }
