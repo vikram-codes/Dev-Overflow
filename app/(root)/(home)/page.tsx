e0104cb3e7c267531923dda787a1d6b1e05f7f6f
Vikram Saini <vikramjit.personal@gmail.com>
2024-06-24 13:49:47 +0530

    Working on Homepage

--- app/(root)/(home)/page.tsx
+++ app/(root)/(home)/page.tsx
@@ -1,13 +1,16 @@
 import { Button } from "@/components/ui/button";
+import Link from "next/link";
 
 export default function Home() {
   return (
     <>
       <div className="flex justify-between">
-        <h1 className="h1-bold text-dark300_light900">All Questions</h1>
-        <Button className="primary-gradient text-light-900 w-36 p-[-2px] pt-6 pb-6 ml-48">
-          Ask a Question
-        </Button>
+        <h1 className="h1-bold text-dark100_light900">All Questions</h1>
+        <Link href="/ask-question">
+          <Button className="primary-gradient !text-light-900 py-3 px-4 min-h-[46px]">
+            Ask a Question
+          </Button>
+        </Link>
       </div>
     </>
   );
