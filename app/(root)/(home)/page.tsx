5daa03a78c522d0d98a74c1e68445cf358472a53
Vikram Saini <vikramjit.personal@gmail.com>
2024-06-25 14:29:13 +0530

    Creating Filters for Homepage

--- app/(root)/(home)/page.tsx
+++ app/(root)/(home)/page.tsx
@@ -1,9 +1,17 @@
 import Filters from "@/components/shared/Filters";
 import LocalSearch from "@/components/shared/search/LocalSearch";
+import { Badge } from "@/components/ui/badge";
 import { Button } from "@/components/ui/button";
 import Link from "next/link";
 
 export default function Home() {
+  const filters = [
+    { title: "Newest" },
+    { title: "Recommended" },
+    { title: "Frequent" },
+    { title: "Unanswered" },
+  ];
+
   return (
     <>
       <div className="flex w-full flex-col-reverse justify-between gap-4 sm:flex-row sm:items-center">
@@ -22,7 +30,20 @@ export default function Home() {
           imgSrc="/assets/icons/search.svg"
           otherClasses="flex-1"
         />
-        <Filters />
+        <div className="flex max-md:hidden justify-between px-2 gap-7">
+          {filters.map((title) => (
+            <Badge
+              key={title.title}
+              className="subtle-medium background-light800_dark300 text-light400_light500 justify-start rounded-md border-none px-6 py-3 uppercase"
+            >
+              {title.title}
+            </Badge>
+          ))}
+        </div>
+
+        <div className="md:hidden">
+          <Filters placeholder="Select a Filter" />
+        </div>
       </div>
     </>
   );
--- components/shared/Filters.tsx
+++ components/shared/Filters.tsx
@@ -1,5 +1,27 @@
 import React from "react";
+import {
+  Select,
+  SelectContent,
+  SelectItem,
+  SelectTrigger,
+  SelectValue,
+} from "@/components/ui/select";
 
-export default function Filters() {
-  return <div>Filters</div>;
+interface Props {
+  placeholder: string;
+}
+
+export default function Filters({ placeholder }: Props) {
+  return (
+    <Select>
+      <SelectTrigger className="w-[180px]">
+        <SelectValue placeholder={placeholder} />
+      </SelectTrigger>
+      <SelectContent>
+        <SelectItem value="light">Light</SelectItem>
+        <SelectItem value="dark">Dark</SelectItem>
+        <SelectItem value="system">System</SelectItem>
+      </SelectContent>
+    </Select>
+  );
 }
