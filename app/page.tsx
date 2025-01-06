78b4dca4d9d30d29e40c67cb2ad884147ecaf1c2
c0944618 <c0944618@mylambton.ca>
2025-01-06 13:12:34 -0500

    Eslint & Prettier Setup

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,7 @@
 export default function Home() {
   return (
-    <h1 className="text-3xl text-violet-700 font-black">Welcome to Next.js</h1>
+    <h1 className="text-3xl text-violet-700 font-black">
+      Welcome to Next.js fake
+    </h1>
   );
 }
