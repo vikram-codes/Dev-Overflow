c02c3e04d78c5e86774bdbda8f0a3f50e56858ee
vikram-codes <vikramjit.personal@gmail.com>
2025-01-07 12:07:49 -0500

    Assets & Metadata

--- app/layout.tsx
+++ app/layout.tsx
@@ -15,8 +15,12 @@ const spaceGrotesk = localFont({
 });
 
 export const metadata: Metadata = {
-  title: "Dev-Overflow",
-  description: "Better version of StackOverflow",
+  title: "Dev Overflow",
+  description:
+    "A community-driven platform for asking and answering programming questions. Get help, share knowledge, and collaborate with developers from around the world. Explore topics in web development, mobile app development, algorithms, data structures, and more.",
+  icons: {
+    icon: "/images/site-logo.svg",
+  },
 };
 
 export default function RootLayout({
