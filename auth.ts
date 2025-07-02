85926f5484d8dca9a4457ece23bc0b555c021fd0
vikram-codes <vikramjit.personal@gmail.com>
2025-07-02 00:16:13 -0400

    Implementing Google SignIn/LogOut

--- auth.ts
+++ auth.ts
@@ -1,6 +1,7 @@
 // auth.ts
 import NextAuth from "next-auth";
 import GitHub from "next-auth/providers/github";
+import Google from "next-auth/providers/google";
 
 import { authConfig } from "./auth.config";
 
@@ -10,6 +11,10 @@ export const { auth, handlers, signIn, signOut } = NextAuth({
     GitHub({
       clientId: process.env.AUTH_GITHUB_ID!,
       clientSecret: process.env.AUTH_GITHUB_SECRET!,
+    }),
+    Google({
+      clientId: process.env.AUTH_GOOGLE_ID!,
+      clientSecret: process.env.AUTH_GOOGLE_SECRET!,
     }),
   ],
   secret: process.env.AUTH_SECRET,
