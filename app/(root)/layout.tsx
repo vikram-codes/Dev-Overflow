f7f8fe62d221751933be2b3746d6c990229aa7ba
vikram-codes <vikramjit.personal@gmail.com>
2025-07-02 15:18:23 -0400

    Implementing LeftSidebar

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -1,12 +1,20 @@
 import { ReactNode } from "react";
 
+import LeftSidebar from "@/components/navigation/LeftSidebar";
 import Navbar from "@/components/navigation/navbar";
 
 const RootLayout = ({ children }: { children: ReactNode }) => {
   return (
-    <main>
+    <main className="background-light900_dark100 relative">
       <Navbar />
-      {children}
+
+      <div className="flex">
+        <LeftSidebar />
+
+        <section className="flex min-h-screen flex-1 flex-col px-6 pb-6 pt-36 max-md:pb-14 sm;px-14">
+          <div className="mx-auto w-full max-w-5xl">{children}</div>
+        </section>
+      </div>
     </main>
   );
 };
---
+++ components/navigation/LeftSidebar.tsx
@@ -0,0 +1,53 @@
+import Image from "next/image";
+import Link from "next/link";
+
+import ROUTES from "@/constants/routes";
+
+import NavLinks from "./navbar/NavLinks";
+import { Button } from "../ui/button";
+
+function LeftSidebar() {
+  return (
+    <secion className="custom-scrollbar background-light900_dark200 light-border sticky left-0 top-0 h-screen flex flex-col justify-between overflow-y-auto border-r p-6 pt-36 shadow-light-300 dark:shadow-none max-sm:hidden lg:w-[266px]">
+      <div className="flex flex-1 flex-col gap-6">
+        <NavLinks />
+      </div>
+
+      <div className="flex flex-col gap-3">
+        <Button
+          className="small-medium btn-secondary min-h-[41px] w-full rounded-lg px-4 py-3 shadow-none"
+          asChild
+        >
+          <Link href={ROUTES.SIGN_IN}>
+            <Image
+              src="/icons/account.svg"
+              alt="Account"
+              width={20}
+              height={20}
+              className="invert-colors lg:hidden"
+            />
+            <span className="primary-text-gradient max-lg:hidden">Log In</span>
+          </Link>
+        </Button>
+
+        <Button
+          className="small-medium light-border-2 text-dark-400_light900  btn-tertiary min-h-[41px] w-full rounded-lg px-4 py-3 shadow-none"
+          asChild
+        >
+          <Link href={ROUTES.SIGN_UP}>
+            <Image
+              src="/icons/sign-up.svg"
+              alt="Account"
+              width={20}
+              height={20}
+              className="invert-colors lg:hidden"
+            />
+            <span className="max-lg:hidden">Sign Up</span>
+          </Link>
+        </Button>
+      </div>
+    </secion>
+  );
+}
+
+export default LeftSidebar;
