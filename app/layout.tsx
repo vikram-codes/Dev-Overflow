dc73c505cb93329a914b75ad0fa15f2da9fdc4a3
vikram-codes <vikramjit.personal@gmail.com>
2025-07-04 17:47:54 -0400

    Adding Filters in HomePage

--- app/layout.tsx
+++ app/layout.tsx
@@ -22,7 +22,7 @@ const spaceGrotesk = localFont({
 });
 
 export const metadata: Metadata = {
-  title: "DevFlow",
+  title: "Dev-OverFlow",
   description:
     "A community-driven platform for asking and answering programming questions. Get help, share knowledge, and collaborate with developers from around the world. Explore topics in web development, mobile app development, algorithms, data structures, and more.",
   icons: {
